a19f3742c385968876310711753b4625dec4cfe0
Oleg Galaburda <burdiuz@gmail.com>
2015-08-03 00:41:29 +0300

    fix jasmine config

--- karma.jasmine.js
+++ karma.jasmine.js
@@ -4,7 +4,7 @@ module.exports = function(config) {
   data.frameworks = ['jasmine'];
   data.logLevel = config.LOG_INFO;
   data.files.push(
-    'tests/1.3/jasmine/*.js'
+    'tests/1.3/jasmine/**/*.js'
   );
   config.set(data);
 };
